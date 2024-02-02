f2203bf0358ed873ce291e24fd671e459b081e32
tris <tris.bouden@gmail.com>
2024-02-02 20:59:26 +0100

    setting up vue app

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# frontend
+# focusty
 
 ## Project setup
 ```
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -12,6 +12,14 @@
     <ul>
       <li>
         <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+          target="_blank"
+          rel="noopener"
+          >babel</a
+        >
+      </li>
+      <li>
+        <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
           target="_blank"
           rel="noopener"
