b3bde0ba34d7e02b2ff3c929d837f50e43ac607d
Tris Dev <97057835+trisDeveloper@users.noreply.github.com>
2024-02-21 20:56:37 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/focusty'
+  base: '/focusty',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
